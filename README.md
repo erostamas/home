9c6a11e444be2c107322fa8a6df264dc07500198
Tamas Eros <tamas.eros1@gmail.com>
2020-03-27 20:22:59 +0100

    Initial commit, importing jcommon UdpSender works, test apllication send ts every second

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# home
+# home client application
+
+# BUILD
+home/env/buildenv/build.sh builds the application
+
+# RUN FROM REPO
+cd home/build
+java -classpath '/usr/lib/erostamas/jcommon.jar:./' Home 192.168.1.181 50002
---
+++ env/buildenv/build.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+cd $(dirname $0)/..
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+cd $ROOT_DIR
+mkdir -p build
+rm -rf build/*
+#find ./src/ -name *.java | xargs javac -d build/ -classpath ".:/usr/lib/erostamas/jcommon.jar"
+javac -verbose -d build/ -classpath "/usr/lib/erostamas/jcommon.jar" src/Home.java
